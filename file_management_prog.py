655bc385cc9f1b49f99cd97ef815d203e560b92b
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-11-19 14:22:40 +0800

    Fixed directory and event handler errors. Still testing..

--- file_management_prog.py
+++ file_management_prog.py
@@ -8,21 +8,23 @@
 # for videos =  mp4, mov
 
 # Accessing files in python
-import os as my_computer
-from os import rename
-from os.path import splitext, join, exists
-from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from os import scandir, rename
+from os.path import splitext, exists, join
+from shutil import move
+from time import sleep
+
 import logging
-import time
-import shutil as move
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 # directories
 my_downloads = "C:/Users/adrian/Downloads"
+
 my_images = "C:/Users/adrian/Desktop/Images"
-my_videos = "C:/Users/adrian/Desktop/Videos"
-my_documents = "C:/Users/adrian/Desktop/Documents"
-my_musics = "C:/Users/adrian/Desktop/Musics"
+# my_videos = "C:/Users/adrian/Desktop/Videos"
+# my_documents = "C:/Users/adrian/Desktop/Documents"
+# my_musics = "C:/Users/adrian/Desktop/Musics"
 
 # Formats
 image_formats = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", 
@@ -39,55 +41,33 @@ music_formats = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
                 ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
 
 def create_newname(location, name):
-    filename, format =  splitext(name) # separates filename including the format
+    filename, extension = splitext(name)
     counter = 1
-    while exists(location, name):
-        name = filename + str(f"({counter})") + format
+    while exists(f"{location}/{name}"):
+        name = f"{filename}({str(counter)}){extension}"
         counter += 1
     return name
-    
-def move_me(location, files_entry, name):
-    if exists(location, name):
+
+def move_me(location, file_entry, name):
+    if exists(f"{location}/{name}"):
         new_name = create_newname(location, name)
         same_name = join(location, name)
         new_file_name = join(location, new_name)
         rename(same_name, new_file_name)
-    move(files_entry, my_images)
+    move(file_entry, location)
 
-class EventHandler(LoggingEventHandler):
-    def events_on_download(self):
-        # scandir method is used to access the directory of a certain path
-        with scandir(my_downloads) as my_computer:
-            for file_entry in my_computer:
-                name = file_entry.name
-                self.move_images(self, file_entry, name)
-                self.move_videos(self, file_entry, name)
-                self.move_music(self, file_entry, name)
-                self.move_documents(self, file_entry, name)
+class EventHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        with scandir(my_downloads) as my_files:
+            for files in my_files:
+                name = files.name
+                self.move_images(files, name)
 
-    def move_images(self, file_entry, name):
+    def move_images(self, file_entry, name):  # * Checks all Image Files
         for img_format in image_formats:
-            if name.endswith(img_format):
+            if name.endswith(img_format) or name.endswith(img_format.upper()):
                 move_me(my_images, file_entry, name)
-                logging.info(name)
-
-    def move_videos(self, file_entry, name):
-        for vid_format in video_formats:
-            if name.endswith(vid_format):
-                move_me(my_videos, file_entry, name)
-                logging.info(name)
-
-    def move_music(self, file_entry, name):
-        for msc_format in music_formats:
-            if name.endswith(msc_format):
-                move_me(my_musics, file_entry, name)
-                logging.info(name)
-
-    def move_documents(self, file_entry, name):
-        for docu_format in documents_formats:
-            if name.endswith(docu_format):
-                move_me(my_documents, file_entry, name)
-                logging.info(name)
+                logging.info(f"Moved image file: {name}")
 
 # if statement for the directory to run as a script
 if __name__ == "__main__":
@@ -105,7 +85,7 @@ if __name__ == "__main__":
     observer.start() # execute the observer
     try:
         while True:
-            time.sleep(10)
+            sleep(10)
     except KeyboardInterrupt:
         observer.stop() # to not break/exit the program for any keyboard input interruption
     observer.join()
