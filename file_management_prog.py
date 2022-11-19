2b749bc8633ece20c462658b8000b6f192087ea5
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-11-19 11:44:18 +0800

    Watchdog for events in the directory implemented

--- file_management_prog.py
+++ file_management_prog.py
@@ -3,8 +3,31 @@
 
 # Accessing files in python
 import os as my_computer
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+import logging
 
 my_downloads = "C:/Users/adrian/Downloads"
 # scandir method is used to access the directory of a certain path
 for files in my_computer.scandir(my_downloads):
     print(files.name)
+    
+
+# if statement for the directory to run as a script 
+if __name__ == "__main__":
+    # format for the logging info
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s -%(message)s",
+        datefmt="%Y-%m-%d %H:%M"
+    )
+    # for the path
+    path = my_downloads
+    # for any modifications/deletions/creation
+    event_handler = LoggingEventHandler()
+    # observe
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+
+    # execute the observer
+    observer.start()
