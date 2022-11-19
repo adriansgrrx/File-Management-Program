9b1c05c9e3a8670d49f393f6b9bedf4ba352e3b0
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-11-19 13:31:09 +0800

    Finished the formats of my files. Testing coming up.

--- file_management_prog.py
+++ file_management_prog.py
@@ -17,14 +17,26 @@ import logging
 import time
 import shutil as move
 
+# directories
 my_downloads = "C:/Users/adrian/Downloads"
 my_images = "C:/Users/adrian/Desktop/Images"
+my_videos = "C:/Users/adrian/Desktop/Videos"
+my_documents = "C:/Users/adrian/Desktop/Documents"
+my_musics = "C:/Users/adrian/Desktop/Musics"
 
 # Formats
 image_formats = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", 
                     ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw",".k25", ".bmp", 
                     ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", 
                     ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
+
+video_formats = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
+
+documents_formats = [".doc", ".docx", ".odt",
+                    ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
+
+music_formats = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
+                ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
 
 def create_newname(location, name):
     filename, format =  splitext(name) # separates filename including the format
@@ -49,10 +61,31 @@ class EventHandler(LoggingEventHandler):
             for file_entry in my_computer:
                 name = file_entry.name
                 self.move_images(self, file_entry, name)
+                self.move_videos(self, file_entry, name)
+                self.move_music(self, file_entry, name)
+                self.move_documents(self, file_entry, name)
 
     def move_images(self, file_entry, name):
         for img_format in image_formats:
             if name.endswith(img_format):
+                move_me(my_images, file_entry, name)
+                logging.info(name)
+
+    def move_videos(self, file_entry, name):
+        for vid_format in video_formats:
+            if name.endswith(vid_format):
+                move_me(my_images, file_entry, name)
+                logging.info(name)
+
+    def move_music(self, file_entry, name):
+        for msc_format in music_formats:
+            if name.endswith(msc_format):
+                move_me(my_images, file_entry, name)
+                logging.info(name)
+
+    def move_documents(self, file_entry, name):
+        for docu_format in documents_formats:
+            if name.endswith(docu_format):
                 move_me(my_images, file_entry, name)
                 logging.info(name)
 
@@ -74,5 +107,5 @@ if __name__ == "__main__":
         while True:
             time.sleep(10)
     except KeyboardInterrupt:
-        observer.stop() # to break/exit the program for any keyboard input interruption
+        observer.stop() # to not break/exit the program for any keyboard input interruption
     observer.join()
